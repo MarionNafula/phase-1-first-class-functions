e6fe63fff2138aaf91d699b1f3ab282f39b51b55
MarionNafula <wekesamarion100@gmail.com>
2022-12-06 14:01:42 +0300

    Complete assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function receivesAFunction(callback) {
+    return callback();
+}
+
+receivesAFunction(() =>{
+
+});
+
+function returnsANamedFunction() {
+    let sam = () => console.log("mans")
+    return (sam)
+}
+
+var returnsAnAnonymousFunction = function() {
+    return (() => {
+    alert('This an anonymous function')
+    }
+    )
+}
